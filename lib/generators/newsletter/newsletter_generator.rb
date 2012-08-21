fb53dca8e3cd1d96972ef7ec4143a8e6ecd47fcf
basgys <basgys@gmail.com>
2012-08-21 11:03:32 +0200

    Update newsletter generator
    - Add namespace support
    - Generate views
    - Template newsletter file

--- lib/generators/newsletter/newsletter_generator.rb
+++ lib/generators/newsletter/newsletter_generator.rb
@@ -3,7 +3,21 @@ class NewsletterGenerator < Rails::Generators::NamedBase
 	check_class_collision :suffix => "Newsletter"
 	source_root File.expand_path("../templates", __FILE__)
 
-	def create_controller_files
+	def create_newsletter_file
 		template 'newsletter.rb', File.join('app/newsletters', class_path, "#{file_name}_newsletter.rb")
 	end
+
+	def create_view_files
+		actions.each do |action|
+			available_formats.each do |format|
+				create_file  File.join('app/views', class_path, "#{file_name}_newsletter", "#{action}.#{format}.emv"), "#{action}.#{format}"
+			end			
+		end
+	end	
+
+	protected
+
+	def available_formats
+		%w(html text)
+	end	
 end
--- lib/generators/newsletter/templates/newsletter.rb
+++ lib/generators/newsletter/templates/newsletter.rb
@@ -1,3 +1,15 @@
+<% module_namespacing do -%>
 class <%= class_name %>Newsletter < Emailvision4rails::AbstractNewsletter
+<% actions.each do |action| -%>
 
+  def <%= action %>
+    @greeting = "Hi"
+
+		newsletter(
+			:message => {:name => "<%= action %>"},
+			:campaign => {:send_date => Time.now+10.minutes, :mailinglist_id => 123}
+		)	
+  end
+<% end -%>
 end
+<% end -%>
