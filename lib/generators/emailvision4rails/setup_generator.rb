b15e35e751960e8a94237ddaf219ccaf942fe288
basgys <basgys@gmail.com>
2012-08-21 14:46:06 +0200

    Transfer setup generator from emailvision gem

---
+++ lib/generators/emailvision4rails/setup_generator.rb
@@ -0,0 +1,11 @@
+module Emailvision4rails
+	class Setup < Rails::Generators::Base
+
+		source_root File.expand_path('../templates', __FILE__)
+
+		def copy_config_template
+			copy_file "emailvision.yml", "config/emailvision.yml"
+		end
+
+	end
+end
