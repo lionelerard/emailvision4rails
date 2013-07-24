3e41e45a436c6f77f747d318bc05f6f4a2af89c1
lionel <lionel@eboutic.ch>
2013-07-24 11:18:40 +0200

    Try to fix

--- lib/emailvision4rails/emv_handler.rb
+++ lib/emailvision4rails/emv_handler.rb
@@ -9,7 +9,7 @@ module Emailvision4rails
 		def self.call(template)
 			compiled_source = erb_handler.call(template)
 			if template.formats.include?(:html)
-        "Roadie::Inliner.new(Roadie::AssetPipelineProvider.new, [], #{compiled_source}, {}, nil).execute"
+        "Roadie::Inliner.new(Roadie::AssetPipelineProvider.new, [], begin;#{compiled_source};end, {}, nil).execute"
 			else
 				compiled_source
 			end
